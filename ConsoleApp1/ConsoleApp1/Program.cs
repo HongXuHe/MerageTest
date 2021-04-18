7605a9e01194b0f00c4017e0f92ccddb2ca3a1e2
HongXuHe <matt917917@gmail.com>
2021-04-18 10:47:38 +1000

    Update Program.cs
    Dev mainbranch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Main Branch");
+            Console.WriteLine("Hello World! Dev main Branch");
         }
     }
 }
